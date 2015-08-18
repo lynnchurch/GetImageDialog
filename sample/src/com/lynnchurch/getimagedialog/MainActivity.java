6f62e24b189edece29f2cf5788d7bb4fb1326522
lynnchurch <lynnchurch@163.com>
2015-08-18 20:49:23 +0800

    commit

--- sample/src/com/lynnchurch/getimagedialog/MainActivity.java
+++ sample/src/com/lynnchurch/getimagedialog/MainActivity.java
@@ -6,7 +6,6 @@ import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
-import android.widget.ImageButton;
 import android.widget.ImageView;
 
 import com.lynnchurch.alertdialogdemo.R;
